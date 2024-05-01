d53d37c765da3d10ef840078e0b49b1a0c5f12a2
mariloptripel <marisol_e.lopez@yahoo.com.ar>
2024-05-01 00:13:18 -0300

    done

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,5 @@
 const currentYear = new Date().getFullYear();
-const currentYearParagraph = document.querySelector('footer p:first-of-type');
-currentYearParagraph.textContent = `© ${currentYear}`;
+document.getElementById('currentyear').textContent = `© ${currentYear}`;
 
 const lastModifiedDate = document.lastModified;
-const lastModifiedParagraph = document.querySelector('footer #lastModified');
-lastModifiedParagraph.textContent = `Last modified: ${lastModifiedDate}`;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModifiedDate}`;
