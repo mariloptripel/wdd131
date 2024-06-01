53a43d14a37bbd8cd807670b0f92b505d24bbcd9
mariloptripel <marisol_e.lopez@yahoo.com.ar>
2024-06-01 17:45:22 -0300

    done

--- project/scripts/contact.js
+++ project/scripts/contact.js
@@ -1,4 +1,3 @@
-// Footer
 document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     document.getElementById('currentyear').textContent = currentYear;
@@ -6,3 +5,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const lastModified = document.lastModified;
     document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
 });
+
+// Products
+const products = [
+    {
+        id: "beginner",
+        name: "Beginner",
+        avgRating: 4.5
+    },
+    {
+        id: "complete",
+        name: "Complete",
+        avgRating: 4.7
+    },
+    {
+        id: "enterprise",
+        name: "Enterprise",
+        avgRating: 4.0
+    }
+];
+
+// Product Name Function
+function populateProductOptions() {
+    const productNameSelect = document.getElementById('subscription'); // Change the ID to match your select element
+    products.forEach(product => {
+        const option = document.createElement('option');
+        option.textContent = product.name; 
+        option.value = product.id; 
+        productNameSelect.appendChild(option);
+    });
+}
+
+// Populate product options
+populateProductOptions();
